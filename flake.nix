224a12cdc0499048b6ef9ee1d1753512f58575d0
haruki7049 <tontonkirikiri@gmail.com>
2025-10-03 12:06:09 +0900

    chore(devShells.default): Append deno

--- flake.nix
+++ flake.nix
@@ -52,6 +52,9 @@
               # Editor
               pkgs.vim
 
+              # denops.vim dependency
+              pkgs.deno
+
               # Plugin fetcher
               pkgs.git
             ];
